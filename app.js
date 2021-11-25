4c15026c43620ea59fc953bd5d4857b7c7cf83ca
Sofirul Danatriya <sofiruld@mail.ugm.ac.id>
2021-11-25 22:17:54 +0700

    add mymission page, load mission feature

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var misiRouter = require('./routes/misi');
+var myMissionRouter = require('./routes/myMission');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/misi', misiRouter);
+app.use('/mymission', myMissionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
